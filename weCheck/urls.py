80abbc2e516d9d1eaef5224b837fb8f23b979b95
stone <1453793807@qq.com>
2018-08-05 12:59:26 +0800

    urls patterns

--- weCheck/urls.py
+++ weCheck/urls.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from django.urls import path,include
+from weCheck import views
 
 app_name = 'weCheck'
 urlpatterns = [
+
+    path('login/',           views.login,          name='login'),           #access: all
+    path('logout/',          views.logout,         name='logout'),          #access: all
+    path('register/',        views.register,       name='register'),        #access: all
+    path('user/',            views.user,           name='user'),            #access: all
+    path('group/',           views.group,          name='group'),           #access: all
+    path('group/list/',      views.grouplist,      name='grouplist'),       #access: all
+    path('group/add/',       views.groupadd,       name='groupadd'),        #access: manager
+    path('group/join/',      views.groupjoin,      name='groupjoin'),       #access: user
+    path('group/quit/',      views.groupquit,      name='groupquit'),       #access: user
+    path('group/update/',    views.groupupdate,    name='groupupdate'),     #access: manager
+    path('group/delete/',    views.groupdelete,    name='groupdelete'),     #access: manager
+    path('check/status/',    views.checkstatus,    name='checkstatus'),     #access: user
+    path('check/check/',     views.checkcheck,     name='checkcheck'),      #access: user
+    path('check/enable/',    views.checkenable,    name='checkenable'),     #access: manager
+    path('check/disable/',   views.checkdisable,   name='checkdiable'),     #access: manager
+    path('schedule/',        views.schedule,       name='schedule'  ),      #access: manager
+    path('schedule/add/',    views.scheduleadd,    name='scheduleadd'),     #access: manager
+    path('schedule/update/', views.scheduleupdate, name='scheduleupdate'),  #access: manager
+    path('schedule/delete/', views.scheduledelete, name='scheduledelete'),  #access: manager
+
+
 
 ]
--- weCheck/views.py
+++ weCheck/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def register(request):
+    pass
+
+def user(request):
+    pass
+
+def group(request):
+    pass
+
+def grouplist(request):
+    pass
+
+def groupadd(request):
+    pass
+
+
+def groupjoin(request):
+    pass
+
+
+def groupquit(request):
+    pass
+
+
+def groupupdate(request):
+    pass
+
+
+def groupdelete(request):
+    pass
+
+
+def checkstatus(request):
+    pass
+
+
+def checkcheck(request):
+    pass
+
+
+def checkenable(request):
+    pass
+
+
+def checkdisable(request):
+    pass
+
+
+def schedule(request):
+    pass
+
+
+def scheduleadd(request):
+    pass
+
+
+def scheduleupdate(request):
+    pass
+
+
+def scheduledelete(request):
+    pass
+
