7ea19ddee512b24fc144e68bd607c7bbb3573319
stone <1453793807@qq.com>
2018-08-06 13:35:42 +0800

    interface login/logout/register by stone

--- weCheck/models.py
+++ weCheck/models.py
@@ -14,12 +14,24 @@ userType   Integer     用户身份
 
 """
 class user(models.Model):
+
     username  = models.CharField(max_length=30)
     passwd    = models.CharField(max_length=32)
     name      = models.CharField(max_length=10)
     profile   = models.CharField(max_length=100)
     userType  = models.IntegerField(max_length=1)
     isDelete  = models.BooleanField(default=False)
+
+    @classmethod
+    def userObject(cls,username,passwd,name,profile,userType):
+        userNew = user()
+        userNew.username = username
+        userNew.name = name
+        userNew.passwd = passwd
+        userNew.profile = profile
+        userNew.userType = userType
+        userNew.save()
+        return userNew
 
 """
             tokon表
@@ -33,9 +45,18 @@ domain       string      有效时间
 """
 class userToken(models.Model):
 
-    userID    = models.ForeignKey(user,related_name='username')
+    username  = models.ForeignKey(user,related_name='username')
     token     = models.CharField(max_length=32)
     domain    = models.CharField(max_length=30)
+
+    @classmethod
+    def userTokenObject(cls,username,token,domain=None):
+        new = userToken()
+        new.username = username
+        new.token = token
+        new.domain = domain
+        new.save()
+        return new
 
 
 """
@@ -56,6 +77,15 @@ class group(models.Model):
     member    = models.CharField(max_length=1000)
     isDelete    = models.BooleanField(default=False)
 
+    @classmethod
+    def groupObject(cls,groupID,name,owner,member=None):
+        new = group()
+        new.groupID = groupID
+        new.name = name
+        new.owner = owner
+        new.member = member
+        new.save()
+        return new
 
 """
              群组表
@@ -80,6 +110,19 @@ class check(models.Model):
     enable      = models.BooleanField(default=False)
     results     = models.CharField(max_length=1000)
     members     = models.CharField(max_length=1000)
+
+    @classmethod
+    def checkObject(cls,checkID,groupID,startUpTime=None,duration=None,enable=None,results=None,members=None):
+        new = check()
+        new.checkID = checkID
+        new.groupID = groupID
+        new.startUpTime = startUpTime
+        new.duration = duration
+        new.enable = enable
+        new.results = results
+        new.members = members
+        new.save()
+        return new
 
 
 """
@@ -108,6 +151,16 @@ class checkPlan(models.Model):
     enable      = models.BooleanField(default=False)
     isDelete    = models.BooleanField(default=False)
 
-
+    @classmethod
+    def checkPlanObejct(cls,planID,groupID,startUpTime=None,duration=None,repeat=None,enable=False):
+        new = checkPlan()
+        new.planID = planID
+        new.groupID = groupID
+        new.startUpTime = startUpTime
+        new.duration = duration
+        new.repeat =repeat
+        new.enable = enable
+        new.save()
+        return new
 
 
--- weCheck/views.py
+++ weCheck/views.py
@@ -1,18 +1,89 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password,check_password
+from django.contrib.auth import logout
+from weCheck import models
+from django.http import JsonResponse
+from HTML5project import settings
+import base64
+import os
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
 
 def login(request):
-    pass
+    error = []
+    if request.is_ajax() and request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+        userlogin = models.user.objects.filter(username=username)
+        if user is not None:
+            if check_password(password, userlogin.passwd):
+                token = base64.b32encode(os.urandom(20))
+                models.userToken.userTokenObject(username,token)
+                request.session['token'] = token
+                request.session['username'] = username
+                request.session['SESSION_KEY'] = user._meta.pk.value_to_string(user)
+                return JsonResponse({
+                                     'status':200,
+                                    })
+            else:
+                error.append('WRONG password')
+        else:
+            error.append('USER not exist')
+    else:
+        error.append('WRONG request method')
+    return JsonResponse({
+                            'status':202,
+                            'message':error
+                                        })
+
+
+
+
 
 def logout(request):
-    pass
+
+    logout(request)
+
+    return JsonResponse({
+        'status': 200,
+    })
+
 
 def register(request):
-    pass
+    error = []
+    if request.is_ajax() and request.method == 'POST':
+        username = request.POST['username']
+        if models.user.objects.filter(username=username) is None:
+
+            passwd = make_password(request.POST['password'])
+            name = request.POST['name']
+            img = request.FILES['profile']
+            userType = request.POST['userType']
+            profile = settings.STATIC_URL+'weCheck/'+username+'.jpg'
+
+            with open(os.path.join(settings.STATIC_ROOT,'weCheck/img'+username+'.jpg'),'wb') as f:
+                f.write(img)
+
+            models.user.userObject(username,passwd,name,profile,userType)
+
+            return JsonResponse({
+                                 'status':200,
+                                            })
+        else:
+            error.append('REAPEAT of username')
+    else:
+        error.append('WRONG request method')
+    return JsonResponse({
+        'status':202,
+        'message':error,
+    })
 
 def user(request):
-    pass
+    error = []
+    if request.is_ajax() and request.method == 'POST':
+
 
 def group(request):
     pass
