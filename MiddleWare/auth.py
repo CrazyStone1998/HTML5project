9ad405754c16cf9684966bdd637215861731f812
stone <1453793807@qq.com>
2018-08-26 07:52:59 +0800

    ''

--- MiddleWare/auth.py
+++ MiddleWare/auth.py
@@ -20,51 +20,48 @@ class authenticationMiddleWare(MiddlewareMixin):
             requestData = request.GET
         else:
             requestData = request.POST
-        if '.jpg' not in request.path:
-            # 如果用户没有认证，限制访问
-            if not request.session.has_key('sessionID') and not request.session.has_key('token') \
-                    and 'register' not in request.path and 'login' not in request.path:
-                context.append('Please login')
+
+        # 如果用户没有认证，限制访问
+        if not request.session.has_key('sessionID') and not request.session.has_key('token') \
+                and 'register' not in request.path and 'login' not in request.path:
+            context.append('Please login')
+            return JsonResponse({
+                'status': 403,
+                'message': context,
+            })
+        elif request.session.has_key('sessionID') and request.session.has_key('token') \
+                and 'register' not in request.path and 'logout' not in request.path:
+            try:
+                #用户拥有session，登陆验证
+                user = userSystem(request)
+                if not user.getUserObject():
+                    context.append('your authentication exceed the time limit')
+                    return JsonResponse({
+                        'status': 403,
+                        'message': context,
+                    })
+                '''
+
+                权限管理
+
+                pass
+
+
+                '''
+            except Exception as e:
+                context.append('somthing is wrong')
                 return JsonResponse({
-                    'status': 403,
+                    'status': 202,
                     'message': context,
                 })
-            elif request.session.has_key('sessionID') and request.session.has_key('token') \
-                    and 'register' not in request.path and 'logout' not in request.path:
-                try:
-                    #用户拥有session，登陆验证
-                    user = userSystem(request)
-                    if not user.getUserObject():
-                        context.append('your authentication exceed the time limit')
-                        return JsonResponse({
-                            'status': 403,
-                            'message': context,
-                        })
-                    '''
-    
-                    权限管理
-    
-                    pass
-    
-    
-                    '''
-                except Exception as e:
-                    context.append('somthing is wrong')
-                    return JsonResponse({
-                        'status': 202,
-                        'message': context,
-                    })
-
-            # 用户已登录，而且url是login,将转到首页
-            if request.session.has_key('sessionID') and request.session.has_key('token') and 'login' in request.path:
-
-                if request.POST.get('username') == userSystem(request).getUsername():
-
-                    context.append('Repeat login')
-                    return JsonResponse({
-                        'status': 202,
-                        'message': context,
-                    })
+        # 用户已登录，而且url是login,将转到首页
+        if request.session.has_key('sessionID') and request.session.has_key('token') and 'login' in request.path:
+            if request.POST.get('username') == userSystem(request).getUsername():
+                context.append('Repeat login')
+                return JsonResponse({
+                    'status': 202,
+                    'message': context,
+                })
 
 
 
