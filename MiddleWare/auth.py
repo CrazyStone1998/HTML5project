7f9cd50fdc0ccc7cc7bee72768d5744fa56024fa
stone <1453793807@qq.com>
2018-09-11 10:20:28 +0800

    ''

--- MiddleWare/auth.py
+++ MiddleWare/auth.py
@@ -22,46 +22,46 @@ class authenticationMiddleWare(MiddlewareMixin):
             requestData = request.GET
         else:
             requestData = request.POST
-        #
-        #
-        # if 'user' in request.path or 'group' in request.path or 'check' in request.path \
-        #     or 'schedule' in request.path or 'history' in request.path or 'record' in request.path:
-        #     # 如果用户没有认证，限制访问
-        #     if not request.session.has_key('sessionID') and not request.session.has_key('token') \
-        #             and 'register' not in request.path and 'login' not in request.path:
-        #         context = 'Please login'
-        #         return JsonResponse({
-        #             'status': 403,
-        #             'message': context,
-        #         })
-        #     elif request.session.has_key('sessionID') and request.session.has_key('token') \
-        #             and 'register' not in request.path and 'logout' not in request.path \
-        #             and 'login' not in request.path:
-        #         try:
-        #
-        #             #用户拥有session，登陆验证
-        #             user = userSystem(request)
-        #             if not user.getUserObject():
-        #                 #用户登出
-        #                 logout(request)
-        #
-        #                 context = 'your authentication exceed the time limit'
-        #                 return JsonResponse({
-        #                     'status': 403,
-        #                     'message': context,
-        #                 })
-        #             '''
-        #
-        #             权限管理
-        #
-        #             pass
-        #
-        #
-        #             '''
-        #         except Exception as e:
-        #             context = 'somthing is wrong'
-        #             return JsonResponse({
-        #                 'status': 202,
-        #                 'message': context,
-        #             })
-        #
+
+
+        if 'user' in request.path or 'group' in request.path or 'check' in request.path \
+            or 'schedule' in request.path or 'history' in request.path or 'record' in request.path:
+            # 如果用户没有认证，限制访问
+            if not request.session.has_key('sessionID') and not request.session.has_key('token') \
+                    and 'register' not in request.path and 'login' not in request.path:
+                context = 'Please login'
+                return JsonResponse({
+                    'status': 403,
+                    'message': context,
+                })
+            elif request.session.has_key('sessionID') and request.session.has_key('token') \
+                    and 'register' not in request.path and 'logout' not in request.path \
+                    and 'login' not in request.path:
+                try:
+
+                    #用户拥有session，登陆验证
+                    user = userSystem(request)
+                    if not user.getUserObject():
+                        #用户登出
+                        logout(request)
+
+                        context = 'your authentication exceed the time limit'
+                        return JsonResponse({
+                            'status': 403,
+                            'message': context,
+                        })
+                    '''
+
+                    权限管理
+
+                    pass
+
+
+                    '''
+                except Exception as e:
+                    context = 'somthing is wrong'
+                    return JsonResponse({
+                        'status': 202,
+                        'message': context,
+                    })
+
