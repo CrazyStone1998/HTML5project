fd3406968b29c1bd6d390a6e24305991f027e6e4
CrazyStone1998 <38484269+CrazyStone1998@users.noreply.github.com>
2018-09-25 00:29:57 +0800

    Update and rename HTML5project/settings.py to  HTML5project/settings.py

--- HTML5project/settings.py
+++ HTML5project/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'MiddleWare.auth.authenticationMiddleWare',
+    #'MiddleWare.auth.authenticationMiddleWare',
     'django.middleware.cache.FetchFromCacheMiddleware', # This must be last
 ]
 
