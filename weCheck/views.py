ed89912b1cb04533e0604b903965f11c78c8bf58
CrazyStone1998 <38484269+CrazyStone1998@users.noreply.github.com>
2018-11-16 16:29:39 +0800

    Update views.py

--- weCheck/views.py
+++ weCheck/views.py
@@ -1035,7 +1035,7 @@ def checkcheck(request):
                                 "message": "ok"
                             })
                         else:
-                            error = face_result['err_msg']
+                            error = face_result['error_msg']
                             return JsonResponse({
                                 "status": 202,
                                 "message": error
